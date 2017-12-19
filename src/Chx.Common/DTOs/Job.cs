6c73561090634fe371bb798a4e1e383cbddcf77b
Sean Kelly <sean.kelly@iag.co.nz>
2017-12-19 23:44:28 +1300

    Added Job DTO

---
+++ src/Chx.Common/DTOs/Job.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chx.Common.DTOs
+{
+    public enum JobType
+    {
+        HttpRequest,
+        Process
+    }
+
+    public enum JobStatus
+    {
+        Unknown,
+        Ready,
+        Active,
+        Finished,
+        Crashed
+    }
+
+    public class Job
+    {
+        public JobType JobType { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+        public JobStatus JobStatus { get; set; }
+    }
+}
