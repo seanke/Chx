5d1869ef876542742d1975fef832887916420f7b
Sean Kelly <sean.kelly@iag.co.nz>
2017-12-19 23:29:37 +1300

    Added JobsController

---
+++ src/Chx.Worker/Controllers/JobsController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Chx.Worker.Controllers
+{
+    public class JobsController : ApiController
+    {
+        [Route("v1/jobs")]
+        [HttpPost]
+        public HttpResponseMessage RunJob(HttpRequestMessage req)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
