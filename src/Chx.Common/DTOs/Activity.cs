5e8ccb4af99ebd86661f772503aef2c90c78aeb4
Sean Kelly <sean.kelly@iag.co.nz>
2017-12-20 00:00:03 +1300

    Added Activity DTO

---
+++ src/Chx.Common/DTOs/Activity.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chx.Common.DTOs
+{
+    public enum ActivityType
+    {
+        HttpTest,
+        ProcessTest
+    }
+    public class Activity
+    {
+        public string Name { get; set; }
+    }
+}
