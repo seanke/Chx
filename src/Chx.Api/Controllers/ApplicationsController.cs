887a3abe52e781ad1e0dc9831222cf1b2fe8d97a
Sean Kelly <sean.kelly@iag.co.nz>
2017-12-19 23:29:22 +1300

    Added ApplicationsController

---
+++ src/Chx.Api/Controllers/ApplicationsController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Chx.Api.Controllers
+{
+    [RoutePrefix("v1/applications")]
+    public class ApplicationsController : ApiController
+    {
+        [HttpGet]
+        public HttpResponseMessage GetApplicatios(HttpRequestMessage req)
+        {
+            throw new NotImplementedException(); 
+        }
+    }
+}
