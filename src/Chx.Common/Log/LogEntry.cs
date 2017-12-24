285c7c5acf37f934e663319017017b693c185091
Sean Kelly <sean.kelly@iag.co.nz>
2017-12-24 16:54:34 +1300

    Added LogEntry File

---
+++ src/Chx.Common/Log/LogEntry.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Chx.Common.Log
+{
+    public enum LogEntryType
+    {
+        JobStarted,
+        JobCompleted
+    }
+
+    public enum LogEntryLevel
+    {
+        Error,
+        Warning,
+        Infomation,
+        Verbose
+    }
+
+    public class LogEntry
+    {
+        public DateTime DateTime { get; set; }
+        public List<KeyValuePair<string, string>> Messages { get; set; }
+
+        public LogEntry()
+        {
+            Messages = new List<KeyValuePair<string, string>>();
+        }
+    }
+}
